ec8687ded4587dd99d32e8b0db03581ce9a05a93
Alex Fiorenza <alexfiorenza2012@gmail.com>
2025-10-02 14:57:36 -0300

    Feat: Start arena metric & new judges support

--- fair_forge/helpers/judge.py
+++ fair_forge/helpers/judge.py
@@ -1,42 +1,64 @@
+from typing import Optional
 from pydantic import SecretStr
-from fair_forge.helpers.cot import CoT
+from fair_forge.helpers.cot import ChainOfThought, CoT
 import re
 import logging
 import json
+from langchain_core.prompts import ChatPromptTemplate
 
 
 class Judge:
     def __init__(
         self,
-        bos_think_token: str = "<think>",
-        eos_think_token: str = "</think>",
+        bos_think_token: Optional[str] =None,
+        eos_think_token: Optional[str] = None,
         base_url: str = "https://api.groq.com/openai",
         api_key: SecretStr = SecretStr(""),
         model: str = "deepseek-r1-distill-llama-70b",
         temperature: float = 0,
-        bos_json_clause: str = "```json",
-        eos_json_clause: str = "```",
+        bos_json_clause = "```json",
+        eos_json_clause = "```",
     ):
-        self.cot = CoT(
-            bos_think_token=bos_think_token,
-            eos_think_token=eos_think_token,
-            base_url=base_url,
-            api_key=api_key,
-            model=model,
-            temperature=temperature,
-        )
+        if bos_think_token is not None:
+            # if bos_think_token is not None, then we need to use the CoT model
+            self.cot = CoT(
+                bos_think_token=bos_think_token,
+                eos_think_token=eos_think_token,
+                base_url=base_url,
+                api_key=api_key,
+                model=model,
+                temperature=temperature,
+            )
+        else:
+            self.cot = None
+            self.chat_history = []
         self.bos_json_clause = bos_json_clause
         self.eos_json_clause = eos_json_clause
 
+    def direct_inference(self,system_prompt:str, query: str, **kwargs):
+        self.chat_history.append(("human", query))
+        prompt = ChatPromptTemplate.from_messages(
+            [("system", system_prompt), *self.chat_history]
+        )
+        chain = prompt | self.reasoning_model
+        response = chain.invoke(kwargs)
+        return ChainOfThought(thought="", answer=response.content)
+
+
     def check(self, system_prompt: str, query: str, data: dict):
         """
-        Judge an assistant response using Chain of Thought (CoT) reasoning. using an specific criteria
+        Judge a response using an specific criteria
         """
-        response = self.cot.reason(
-            system_prompt=system_prompt,
-            query=query,
-            **data,
-        )
+        if self.cot is not None:
+            response = self.cot.reason(
+                system_prompt=system_prompt,
+                    query=query,
+                    **data,
+            )
+        else:
+            ## Get response directly from the model, without using the CoT model
+            response = self.direct_inference(system_prompt, query, **data)
+
         ## the judge answer is always a json
         json_match = re.search(
             self.bos_json_clause + r"\s*(\{.*?\})\s*" + self.eos_json_clause,
